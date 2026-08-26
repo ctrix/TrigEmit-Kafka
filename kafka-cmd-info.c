a7f7a3e152147d2164f6550e7ba8001b70c4f88e
Massimo Cetra <mcetra@gmail.com>
2026-08-26 01:43:45 +0000

    Drop the INDENT-OFF block that indent kept corrupting

--- kafka-cmd-info.c
+++ kafka-cmd-info.c
@@ -59,14 +59,7 @@ char *kafka_info(UDF_INIT *initid, UDF_ARGS *UNUSED(args), char *UNUSED(result),
     if (kafka_table_get_brokers(brokers, sizeof(brokers)) == 0) {
         brk = "Not Set";
     }
-/* *INDENT-OFF* */
-    written = snprintf(buf, KAFKA_INFO_MAX_LEN,
-	"Version: %s\n"
-	"Brokers: %s\n",
-	PACKAGE_STRING,
-	brk
-    );
-/* *INDENT-ON* */
+    written = snprintf(buf, KAFKA_INFO_MAX_LEN, "Version: %s\n" "Brokers: %s\n", PACKAGE_STRING, brk);
 
     /* snprintf() reports what it would have written, so clamp to what it did */
     if (written < 0) {
